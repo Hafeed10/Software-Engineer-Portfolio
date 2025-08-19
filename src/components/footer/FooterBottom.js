6e513f9e86bf96f4c4dbce4fcfb3e11ea7fa0fb6
Hafeed10 <hafeex@gamil.com>
2025-08-19 11:11:06 +0530

    portfolio website updete

--- src/components/footer/FooterBottom.js
+++ src/components/footer/FooterBottom.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const FooterBottom = () => {
   return (
-    <footer className="w-full py-6 bg-black">
+    <footer className="w-full py-6">
       <p className="text-center text-gray-400 text-sm md:text-base">
         © {new Date().getFullYear()} Hafeed Muhammed. All rights reserved.
       </p>
